7a5d1515b5cf31788ea1179a47c3b41afead1bbc
inu <inu@mac.local>
2023-08-25 00:15:22 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### 参考資料
+- https://zenn.dev/sprout2000/articles/5d7b350c2e85bc
+- https://zenn.dev/lowpaper/articles/89caa5cdddfd89
---
+++ src/web/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <h1>Memo</h1>
+    </div>
+  );
+};
