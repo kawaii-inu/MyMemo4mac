1ec7691180f00ce680c6600223419e6df35c3837
inu <inu@mac.local>
2023-08-26 10:43:20 +0900

    trayにアイコン表示

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
 import path from "path";
-import { BrowserWindow, app } from "electron";
+import { BrowserWindow, app, Tray, Menu, nativeImage } from "electron";
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 400,
-    height: 400,
-    webPreferences: {
-      preload: path.resolve(__dirname, "preload.js"),
-    },
-  });
-  mainWindow.setPosition(1040,10);
-  mainWindow.loadFile("dist/index.html");
-  // mainWindow.webContents.openDevTools({ mode: "detach" });
+    const mainWindow = new BrowserWindow({
+        width: 400,
+        height: 400,
+        webPreferences: {
+            preload: path.resolve(__dirname, "preload.js"),
+        },
+    });
+    mainWindow.setPosition(1040, 10);
+    mainWindow.loadFile("dist/index.html");
 };
 
 app.whenReady().then(() => {
-  createWindow();
+    const img = nativeImage.createFromPath("src/assets/tray24x24.png");
+    let tray = new Tray(img);
+    tray.setToolTip("Tray app");
+    tray.setContextMenu(
+      // TODO: window を全面に持ってくる実装したい
+      Menu.buildFromTemplate([{ label: "Quit", role: "quit" }]),
+    );
+    createWindow();
 });
 
 app.once("window-all-closed", () => app.quit());
--- src/render/index.tsx
+++ src/render/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 createRoot(document.getElementById("root") as Element).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
